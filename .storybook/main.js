bd12789845eea4a5e8333b95eb0bf8de3ce3ca6b
sungwon-cha <mygenie3@naver.com>
2023-06-06 23:07:59 +0900

    vite -> webpack

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,18 +1,21 @@
 /** @type { import('@storybook/react-vite').StorybookConfig } */
 const config = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  staticDirs: ["../public"],
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  staticDirs: ['../public'],
   addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    '@storybook/preset-scss',
   ],
   framework: {
-    name: "@storybook/react-vite",
-    options: {},
+    name: '@storybook/react-webpack5',
+    options: {
+      fastRefresh: true,
+    },
   },
   docs: {
-    autodocs: "tag",
+    autodocs: 'tag',
   },
 };
 export default config;
--- src/stories/TextEffect.stories.tsx
+++ src/stories/TextEffect.stories.tsx
@@ -13,7 +13,7 @@ export default {
 // More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
 export const Primary = {
   args: {
-    message: '가격할인',
+    message: '부대찌개',
     // text: ""
     // primary: true,
     // label: 'Button',
