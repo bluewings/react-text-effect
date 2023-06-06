60054e63c7ff452ffdbdd65855b1879bf9b927b8
sungwon-cha <mygenie3@naver.com>
2023-06-06 18:44:44 +0900

    fix

--- src/components/TextEffect/Sliced/Sliced.tsx
+++ src/components/TextEffect/Sliced/Sliced.tsx
@@ -1,3 +1,4 @@
+import cx from 'classnames';
 import React, { useRef } from 'react';
 import useTextStyle from '../../../hooks/useTextStyle';
 import styles from './Sliced.module.scss';
@@ -13,7 +14,9 @@ function Sliced({ message }: ISlicedProps) {
 
   return (
     <div ref={ref} className={styles.container}>
-      <span style={textSize}>{message}</span>
+      <span className={cx(styles.stroke, styles.shadow)} style={textSize}>
+        {message}
+      </span>
     </div>
   );
 }
